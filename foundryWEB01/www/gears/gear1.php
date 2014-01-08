c49aa568d6318363e83b8af054504dfc7d5dac56
Darley Stephen <darleys@darleys.org>
2014-01-08 11:43:41 -0500

    Virtuous

--- foundryWEB01/www/gears/gear1.php
+++ foundryWEB01/www/gears/gear1.php
@@ -17,7 +17,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
- * @copyright  Copyright (c) 2010 Sydney IT Pty Ltd  Australia. (http://www.sydneyitc.com.au)
+ * @copyright  Copyright (c) 2013 Virtuous Consulting Services.  (http://www.virtuouscs.com)
  * @Owner     Darley Stephen (http://www.darleys.org)
  * @Author     Darley Stephen (http://www.darleys.org)
  */
