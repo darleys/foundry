2bf2c40b799cef72ff778c685a4b2549562000d7
Darley Stephen <darleys@darleys.org>
2014-01-08 12:25:33 -0500

    cast

--- foundryWEB01/www/casts/Cast_First.php
+++ foundryWEB01/www/casts/Cast_First.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ * Foundry
+ *
+ * LICENSE
+ *
+Copyright 2013 Virtuous Consulting Services
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ * @copyright  Copyright (c) 2013 Virtuous Consulting Services.  (http://www.virtuouscs.com)
+ * @Owner     Darley Stephen (http://www.darleys.org)
+ * @Author     Darley Stephen (http://www.darleys.org)
+ */
 class Cast_First extends Foundry_Local_Broker{
     function getImages() {
         $imageRES = json_decode(rawurldecode(base64_decode(Foundry_Curl::doCurl(PROCESS_IMAGES_LINK."/fetchImages@molds_images"))),true);
