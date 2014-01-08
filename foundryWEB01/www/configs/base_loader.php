c69ab22604567fe9286c5c5504bd30dea251838a
Darley Stephen <darleys@darleys.org>
2014-01-08 11:52:21 -0500

    Vitruous

--- foundryWEB01/www/configs/base_loader.php
+++ foundryWEB01/www/configs/base_loader.php
@@ -4,7 +4,7 @@
  *
  * LICENSE
  *
-Copyright 2010 Sydney IT Pty Ltd
+Copyright 2013 Virtuous Consulting Services
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
